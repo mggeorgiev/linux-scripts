90db71abc6cf3d98d541d9994792c2141f33aa39
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2019-10-27 20:22:27 +0200

    Update local_registry.sh

--- local_registry/local_registry.sh
+++ local_registry/local_registry.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-docker pull registry
-docker run -d -p 5000:5000 --restart always --name registry registry:2
+if [ "$1" != "" ]; then
+    if ["$1"] == "help"]; then 
+        echo "Use remove to stop and delete the registry"
+    elif ["$1" == "remove"]; then
+    fi
+else
+      docker pull registry
+      docker run -d -p 5000:5000 --restart always --name registry registry:2
 
-#verify
-sudo netstat -ntlp | grep :5000
+      #verify
+      sudo netstat -ntlp | grep :5000
+fi
