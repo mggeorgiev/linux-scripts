e21bf99aeae7b632959a862ca93442acaa7130af
Martin Georgiev <mggeorgiev@gmail.com>
2019-10-28 20:17:50 +0200

    Update usb_backup.sh

--- backup/usb_backup.sh
+++ backup/usb_backup.sh
@@ -7,24 +7,26 @@ Weekly="Weekly"
 Yearly="Yearly"
 
 if [ "$1" != "" ]; then
-    if [ "$1" == "weekly" ]; then 
+    if [ "$1" == "weekly" ]; then
         sudo mount /dev/sdf1 /attached
-        $target+="Weekly"
-        sudo rsync -avzh --progress $source ${target}${Weekly}
+        target+="Weekly"
+	#echo $target
+	sudo rsync -avzh --progress $source $target
         sudo umount /attached
     elif [ "$1" == "monthly" ]; then
         sudo mount /dev/sdf1 /attached
-        $target+="Monthly"
-        sudo rsync -avzh --progress /backup_local/vhds/ /attached/backup/vhds/Monthly
+        target+="Monthly"
+        sudo rsync -avzh --progress $source $target
         sudo umount /attached
     elif [ "$1" == "yearly" ]; then
         sudo mount /dev/sdf1 /attached
-        sudo rsync -avzh --progress $source ${target}${Yearly}
+        target+="Yearly"
+	sudo rsync -avzh --progress $source $target
         sudo umount /attached
     fi
 else
     echo "Missing parameter"
-    echo "Use bash usb_backup.sh weekly to backup to the weekly directiry"
-    echo "Use bash usb_backup.sh monthly to backup to the monthly directiry"
-    echo "Use bash usb_backup.sh yearly to backup to the yearly directiry"
+    echo "Use 'bash usb_backup.sh weekly' to backup to the weekly directiry"
+    echo "Use 'bash usb_backup.sh monthly' to backup to the monthly directiry"
+    echo "Use 'bash usb_backup.sh yearly' to backup to the yearly directiry"
 fi
