caeef39f1ada1faacb447d8b1931e01f84227f1e
Martin Georgiev <mggeorgiev@gmail.com>
2019-10-28 20:40:49 +0200

    organize folders

--- pkg_bkp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#_now=$(date +"%Y_%m_%d")
-#_file="/nas/ubuntu-files_$_now"
-#echo "Starting backup to $_file..."
-sudo dpkg --get-selections | grep -v deinstall > ubuntu-files_$(date +"%Y_%m_%d")
-
-#sudo dpkg --set-selections <./ubuntu-files
-#
