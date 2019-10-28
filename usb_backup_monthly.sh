e388de88215d8a09f2239ae331fa09dbdc5531af
Martin Georgiev Georgiev <mggeorgiev@gmail.com>
2019-10-28 10:13:17 +0200

    Delete usb_backup_monthly.sh

--- usb_backup_monthly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo mount /dev/sdf1 /attached
-sudo rsync -avzh --progress /backup_local/vhds/ /attached/backup/vhds/Monthly
-sudo umount /attached
-#sudo cp -u -R /backup_local/vhds/ /attached/backup/vhds/Weekly
-#lsblk
-#sudo blkid
-#sudo fdisk -l
