ede219e0007daf3eab5eba0d370b4c394c20edcf
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2019-10-28 10:08:07 +0200

    Update usb_backup.sh

--- usb_backup.sh
+++ usb_backup.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-sudo mount /dev/sdf1 /attached
-sudo rsync -avzh --progress /backup_local/vhds/ /attached/backup/vhds/Weekly
-sudo umount /attached
-#sudo cp -u -R /backup_local/vhds/ /attached/backup/vhds/Weekly
-#lsblk
-#sudo blkid
-#sudo fdisk -l
+
+if [ "$1" != "" ]; then
+    if [ "$1" == "weekly" ]; then 
+        sudo mount /dev/sdf1 /attached
+        sudo rsync -avzh --progress /backup_local/vhds/ /attached/backup/vhds/Weekly
+        sudo umount /attached
+    elif [ "$1" == "monthly" ]; then
+        sudo mount /dev/sdf1 /attached
+        sudo rsync -avzh --progress /backup_local/vhds/ /attached/backup/vhds/Monthly
+        sudo umount /attached
+    elif [ "$1" == "yearly" ]; then
+        sudo mount /dev/sdf1 /attached
+        sudo rsync -avzh --progress /backup_local/vhds/ /attached/backup/vhds/Yearly
+        sudo umount /attached
+    fi
+else
+    echo "Missing parameter"
+    echo "Use bash usb_backup.sh weekly to backup to the weekly directiry"
+    echo "Use bash usb_backup.sh monthly to backup to the weekly directiry"
+    echo "Use bash usb_backup.sh yearly to backup to the weekly directiry"
+fi
