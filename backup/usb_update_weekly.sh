76afd6276844c5916f11189c543309e92937f493
Martin Georgiev Georgiev <mggeorgiev@gmail.com>
2019-10-28 10:13:07 +0200

    Create usb_update_weekly.sh

---
+++ backup/usb_update_weekly.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo mount /dev/sdf1 /attached
+sudo rsync -avzh --progress /backup_local/vhds/ /attached/backup/vhds/Weekly
+sudo umount /attached
+
+#sudo cp -u -R /backup_local/vhds/ /attached/backup/vhds/Weekly
+#lsblk
+#sudo blkid
+#sudo fdisk -l
