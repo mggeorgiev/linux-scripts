7dc450897aa410ca69ce9a64035dc0f4d851d110
Martin Georgiev <mggeorgiev@gmail.com>
2019-10-29 19:28:35 +0200

    fix virsh start bug

--- backup/backup.sh
+++ backup/backup.sh
@@ -4,4 +4,4 @@ log="/home/georgiem/" #$HOME
 
 sudo virsh suspend u-boinc
 sudo rsync -avzh /cache/ /vhd/ /vhdssd/images/ /var/lib/libvirt/images/ /backup_local/vhds --log-file=${log}/.rsyncd.log
-sudo virch resume u-boinc
+sudo virsh resume u-boinc
