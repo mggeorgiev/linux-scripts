cf2492d0d38d164cd99dfdf29af00acb9050ec7a
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2019-10-29 07:45:55 +0200

    Update usb_backup.sh

--- backup/usb_backup.sh
+++ backup/usb_backup.sh
@@ -3,30 +3,35 @@
 source="/backup_local/vhds/"
 target="/attached/backup/vhds/"
 
-Weekly="Weekly"
-Yearly="Yearly"
+case $1 in
+  weekly )    
+  	echo "weekly"
+	target+="Wearly"
+	run="true"
+	;;
+  monthly )   
+  	echo "monthly"
+	target+="Monthly"
+	run="true"
+	;;
+  yearly ) 
+  	echo "yearly"
+	target+="Mearly"
+	run="true"
+	echo $run
+	;;
+   * ) 
+	echo "Missing parameter"
+	echo "Use 'bash usb_backup.sh weekly' to backup to the weekly directiry"
+	echo "Use 'bash usb_backup.sh monthly' to backup to the monthly directiry"
+	echo "Use 'bash usb_backup.sh yearly' to backup to the yearly directiry"
+   	;;
+esac
 
-if [ "$1" != "" ]; then
-    if [ "$1" == "weekly" ]; then
+if [ "$run" == "true"]; then
         sudo mount /dev/sdf1 /attached
-        target+="Weekly"
-	#echo $target
-	sudo rsync -avzh --progress $source $target
-        sudo umount /attached
-    elif [ "$1" == "monthly" ]; then
-        sudo mount /dev/sdf1 /attached
-        target+="Monthly"
+	sudo virsh suspend u-boinc
         sudo rsync -avzh --progress $source $target
+	sudo virch resume u-boinc
         sudo umount /attached
-    elif [ "$1" == "yearly" ]; then
-        sudo mount /dev/sdf1 /attached
-        target+="Yearly"
-	sudo rsync -avzh --progress $source $target
-        sudo umount /attached
-    fi
-else
-    echo "Missing parameter"
-    echo "Use 'bash usb_backup.sh weekly' to backup to the weekly directiry"
-    echo "Use 'bash usb_backup.sh monthly' to backup to the monthly directiry"
-    echo "Use 'bash usb_backup.sh yearly' to backup to the yearly directiry"
 fi
