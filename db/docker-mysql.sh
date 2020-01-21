7b63fdb18dcd2241f6257c8445c6bd61f69ee6b7
mggeorgiev <mggeorgiev@gmail.com>
2020-01-21 23:12:18 +0200

    fix mysql parameters

--- db/docker-mysql.sh
+++ db/docker-mysql.sh
@@ -3,7 +3,6 @@
 value=$(cat ~/userpass.txt)
 
 sudo docker run --name some-mysql \
-	#-v /my/custom:/etc/mysql/conf.d \
-	-v ~/mysql:/var/lib/mysql
+	-v ~/mysql:/var/lib/mysql \
 	-e MYSQL_ROOT_PASSWORD=$value \
 	-d mysql:tag
