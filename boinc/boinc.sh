1a3f3e19f0061442cc6596037e9d08f9103862ca
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-03-19 09:45:37 +0200

    Update boinc.sh

--- boinc/boinc.sh
+++ boinc/boinc.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-for url in $(boinccmd --get_project_status | sed -n 's/\s*master URL: //p'); do
-  boinccmd --project ${url} update;
-done
-#update resume
+case $1 in
+  update )
+  	echo "update"
+	  param1+="update"
+	  run="true"
+	;;
+  reset )
+  	echo "reset"
+	  param1+="reset"
+	  run="true"
+	;;
+  resume )
+  	echo "resume"
+    target+="resume"
+    run="true"
+	;;
+   * )
+	echo "Missing parameter"
+	echo "Use 'bash boinc.sh update' to contact scheduling server"
+	echo "Use 'bash usb_backup.sh reset' to delete current work and get more"
+	echo "Use 'bash usb_backup.sh resume' to resume"
+   	;;
+esac
+
+if [ "$run" == "true" ]; then
+  for url in $(boinccmd --get_project_status | sed -n 's/\s*master URL: //p'); do
+    boinccmd --project ${url} $param1;
+  done
+fi
