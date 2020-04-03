2def9a5f1cd73a49e2540880dd36f10b2fe4a54d
Martin Georgiev <mggeorgiev@gmail.com>
2020-04-03 21:06:46 +0300

    add foreach loop

--- administration/delete_archived_logs.sh
+++ administration/delete_archived_logs.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 FILE=/etc/resolv.conf
-if test -f "$FILE"; then
-    echo "$FILE exist"
-fi
+
+
+#!/bin/bash
+FILES=/backup_local/logs/log_daily/*gz
+for f in $FILES
+do
+  echo "Processing $f file..."
+    if test -f "$FILE"; then
+        echo "$FILE exist"
+    fi  # take action on each file. $f store current file name
+  #cat $f
+done
