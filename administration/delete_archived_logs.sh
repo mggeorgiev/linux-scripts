11663552bd8965d0b01a11c442d67c5d6e1f6af6
Martin Georgiev <mggeorgiev@gmail.com>
2020-04-03 21:08:21 +0300

    Update delete_archived_logs.sh

--- administration/delete_archived_logs.sh
+++ administration/delete_archived_logs.sh
@@ -7,7 +7,7 @@ FILE=/etc/resolv.conf
 FILES=/backup_local/logs/log_daily/*gz
 for f in $FILES
 do
-  echo "Processing $f file..."
+  #echo "Processing $f file..."
     if test -f "$FILE"; then
         echo "$FILE exist"
     fi  # take action on each file. $f store current file name
