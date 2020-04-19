192f282addd49ba93ca91ed8e8c50c1c6f24957e
Martin Georgiev <mggeorgiev@gmail.com>
2020-04-19 10:15:10 +0300

    break apex_report command into multiple  lines

--- db/apex_report.sh
+++ db/apex_report.sh
@@ -1,2 +1,4 @@
 #/bin/bash
-ssh georgiem@apex -t "sudo df -h && echo 'Backup size' && du -hx /u01/app/oracle/fast_recovery_area/XE/backupset/"
+ssh georgiem@apex -t "sudo df -h &&" \
+                    " echo 'Backup size' &&" \
+                    " du -hx /u01/app/oracle/fast_recovery_area/XE/backupset/"
