6ca631d4cb20f38ef51ff2bc6e6eabfff742eb61
“Martin <mggeorgiev@gmail.com>
2020-05-07 19:29:37 +0300

    add journal clear and deploy to local registry

---
+++ local_registry/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+container_name = covid19
+registry = localhost:5000/${container_name}
+
+sudo docker build -t ${container_name} .
+sudo docker ps
+sudo docker commit 1a1ea24a07d3 covid19:latest
+sudo docker tag ${container_name}:latest ${registry}
+sudo docker push ${registry}
+sudo docker run -d -p 8080:80 --name ${container_name} ${registry}
