0c94247e0bbce71d6dda3f6cdd913365f468a3f0
Martin Georgiev <mggeorgiev@gmail.com>
2020-05-07 10:16:17 +0300

    Add condition to update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 sudo apt update
-sudo apt upgrade
+sudo apt list --upgradable
+
+if [ "$1" == "y" ]; then
+	sudo apt upgrade -y
+else
+        # Read User Input
+        echo -n "Do you want to proceed? (y/n):"
+        read -s proceed
+        if [ "$proceed" == "y" ]; then
+                echo "Use remove to stop and delete the registry"
+                sudo apt upgrade -y
+        fi
+
+fi
+
 sudo apt autoremove
