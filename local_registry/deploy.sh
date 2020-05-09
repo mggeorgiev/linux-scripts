2b384756449af872f06e4eb7a43551a672f7eff7
“Martin <mggeorgiev@gmail.com>
2020-05-09 21:48:18 +0300

    auto tag and push images

--- local_registry/deploy.sh
+++ local_registry/deploy.sh
@@ -4,11 +4,9 @@ registry="192.168.88.46:5000/${container_name}"
 
 sudo docker build -t ${container_name} .
 #sudo docker ps -aqf "name=${container_name}"
-sudo docker commit (sudo docker ps -aqf "name=${container_name}") ${container_name}:latest
+#sudo docker commit (sudo docker ps -aqf "name=${container_name}") ${container_name}:latest
 sudo docker tag ${container_name}:latest ${registry}
 sudo docker push ${registry}
 #sudo docker run -d -p 8080:80 --name ${container_name} ${registry}
-
-#curl -H GET http://192.168.88.46:5000/v2/_catalog
-#curl -X DELETE localhost:5000/v1/repositories/ubuntu/tags/latest true
-#curl -X DELETE localhost:5000/v1/repositories/ubuntu/tags/latest true
+#sudo docker tag covid19:latest 192.168.88.46:5000/covid19
+#sudo docker push 192.168.88.46:5000/covid19/latest
