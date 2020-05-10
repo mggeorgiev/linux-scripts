790836fcd28428e40dd5f98ab38dcdda81f10cc0
Martin Georgiev <mggeorgiev@gmail.com>
2020-05-10 06:49:31 +0300

    chaneg restart policy for local registry deployments

--- local_registry/deploy.sh
+++ local_registry/deploy.sh
@@ -7,6 +7,6 @@ sudo docker build -t ${container_name} .
 #sudo docker commit (sudo docker ps -aqf "name=${container_name}") ${container_name}:latest
 sudo docker tag ${container_name}:latest ${registry}
 sudo docker push ${registry}
-#sudo docker run -d -p 8080:80 --name ${container_name} ${registry}
+#sudo docker run -d -p 8080:80 --restart=unless-stopped --name ${container_name} ${registry}
 #sudo docker tag covid19:latest 192.168.88.46:5000/covid19
 #sudo docker push 192.168.88.46:5000/covid19/latest
