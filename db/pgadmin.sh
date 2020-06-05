5a98d2a2a8a4ec85ceb22de8ad04ad59c876b3bd
Martin Georgiev <mggeorgiev@gmail.com>
2020-06-05 23:33:46 +0300

    update pgadimin

--- db/pgadmin.sh
+++ db/pgadmin.sh
@@ -8,9 +8,9 @@ if [ "$(docker ps -q -f name=$container_name)" ]; then
         sudo docker stop $container_name && sudo docker rm $container_name
 fi
 
-docker pull dpage/pgadmin4
+#docker pull dpage/pgadmin4
 docker run --name=$container_name \
-            -p 5050:80 -e 'PGADMIN_DEFAULT_EMAIL=user@martingeorgiev.com' \
-            -e 'PGADMIN_DEFAULT_PASSWORD=$value' \
+            -p 5050:80 -e 'PGADMIN_DEFAULT_EMAIL=mggeorgiev@gmail.com' \
+            -e PGADMIN_DEFAULT_PASSWORD=$value \
             --restart=unless-stopped \
             -d dpage/pgadmin4
