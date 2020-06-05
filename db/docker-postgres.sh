9f12a4ea72eb5d3e016b8843b04546c127698e48
Martin Georgiev <mggeorgiev@gmail.com>
2020-06-05 22:34:56 +0300

    check if a container exists

--- db/docker-postgres.sh
+++ db/docker-postgres.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 value=$(cat ~/userpass.txt)
+container_name="postgres-latest"
 
-sudo docker stop postgres-latest && sudo docker rm postgres-latest
+if [ ! "$(docker ps -q -f name=$container_name)" ]; then
+    #if [ "$(docker ps -aq -f status=exited -f name=$container_name)" ]; then
+        # cleanup
+        sudo docker stop $container_name && sudo docker rm $container_name
+    #fi
+    # run your container
+    #docker run -d --name <name> my-docker-image
+fi
+
 
 $ docker run -d \
-    --name postgres-latest \
+    --name $container_name \
     -e POSTGRES_PASSWORD=$value \
     -v /postgres:/var/lib/postgresql/data \
     postgres
