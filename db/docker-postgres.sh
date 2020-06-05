220beb571116de970fef867e05b8659b740fd15f
Martin Georgiev <mggeorgiev@gmail.com>
2020-06-05 22:41:28 +0300

    fix the logic

--- db/docker-postgres.sh
+++ db/docker-postgres.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 value=$(cat ~/userpass.txt)
-container_name="postgres-latest"
+container_name='postgres-latest'
 
-if [ ! "$(docker ps -q -f name=$container_name)" ]; then
+if [ "$(docker ps -q -f name=$container_name)" ]; then
     #if [ "$(docker ps -aq -f status=exited -f name=$container_name)" ]; then
         # cleanup
         sudo docker stop $container_name && sudo docker rm $container_name
