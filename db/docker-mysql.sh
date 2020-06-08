16ba5c08426154ac63768f47120a57135c9dc66c
“Martin <mggeorgiev@gmail.com>
2020-06-08 10:55:26 +0300

    add certificates to mysql docker deployment

--- db/docker-mysql.sh
+++ db/docker-mysql.sh
@@ -3,9 +3,22 @@
 value=$(cat ~/userpass.txt)
 
 sudo docker stop mysql-latest && sudo docker rm mysql-latest
+#with certs
 sudo docker run --name mysql-latest \
+        --ssl-ca=/etc/ssl/mysql/ca.pem \
+        --ssl-cert=/etc/ssl/mysql/server-cert.pem \
+        --ssl-key=/etc/ssl/mysql/server-key.pem
         -v ~/mysql:/var/lib/mysql \
+        -v ~/mysql/certs:/etc/ssl/mysql \
         -e MYSQL_ROOT_PASSWORD=$value \
         -p 3306:3306 \
         --restart=unless-stopped \
         -d mysql:latest
+
+#backup - no certs
+# sudo docker run --name mysql-latest \
+#         -v ~/mysql:/var/lib/mysql \
+#         -e MYSQL_ROOT_PASSWORD=$value \
+#         -p 3306:3306 \
+#         --restart=unless-stopped \
+#         -d mysql:latest
