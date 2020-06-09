6ff565100bf1fc0f953079696b8e8a4655b5dadb
Martin Georgiev <mggeorgiev@gmail.com>
2020-06-09 21:25:33 +0300

    add_dashboard

---
+++ k8s/k3d/dashboard/add_dashboard.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+kubectl delete ns kubernetes-dashboard
+GITHUB_URL=https://github.com/kubernetes/dashboard/releases
+VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
+kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
+kubectl create -f dashboard.admin-user.yml -f dashboard.admin-user-role.yml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml -f dashboard.admin-user.yml -f dashboard.admin-user-role.yml
+kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token
+kubectl proxy
