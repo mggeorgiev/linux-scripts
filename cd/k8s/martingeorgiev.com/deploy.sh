942ad52780afabc370224e6c6754048d5423f29c
Martin Georgiev <mggeorgiev@gmail.com>
2020-06-13 01:38:51 +0300

    Update folder names

--- cd/k8s/martingeorgiev.com/deploy.sh
+++ cd/k8s/martingeorgiev.com/deploy.sh
@@ -6,4 +6,4 @@ deploymentName="martingeorgiev"
 kubectl create deployment ${deploymentName} --image=${myRepository}/${myImage}:${tag}
 #kubectl scale deployment ${deploymentName} --replicas=3
 kubectl autoscale deployment ${deploymentName} --cpu-percent=80 --min=1 --max=5
-kubectl expose deployment ${deploymentName} --name=${deploymentName}-service --type=LoadBalancer --port 80 --target-port 8080
+kubectl expose deployment ${deploymentName} --name=${deploymentName}-service --type=LoadBalancer --port 8002 --target-port 8080
