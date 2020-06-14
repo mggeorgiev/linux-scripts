5ce75031740efbe0b283a065a544887bca73fa25
Martin Georgiev <mggeorgiev@gmail.com>
2020-06-14 10:18:18 +0300

    Add all folders

--- qemu/qemu-vhd-shrink-all.sh
+++ qemu/qemu-vhd-shrink-all.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+for f in /cache/*.qcow2; do
+    echo "Processing -> $f"
+    file_small="-small"
+    sudo qemu-img convert -O qcow2 $f $f$file_small
+done
+
 for f in /vhd/*.qcow2; do
-    #echo "File -> $f"
-    #echo $f"-small"
+    echo "Processing -> $f"
+    file_small="-small"
+    sudo qemu-img convert -O qcow2 $f $f$file_small
+done
+
+for f in /vhdssd/images/*.qcow2; do
+    echo "Processing -> $f"
     file_small="-small"
     sudo qemu-img convert -O qcow2 $f $f$file_small
 done
