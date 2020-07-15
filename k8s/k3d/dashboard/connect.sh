dcdbb82fb53a71cd7d4e265d7c58a1dfdf1e9f06
“Martin <mggeorgiev@gmail.com>
2020-07-15 14:53:04 +0200

    add export config

--- k8s/k3d/dashboard/connect.sh
+++ k8s/k3d/dashboard/connect.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
+export KUBECONFIG="$(k3d get-kubeconfig --name='mycluster')"
 kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token
 kubectl proxy --address='0.0.0.0' --port=8001 --accept-hosts='.*'
