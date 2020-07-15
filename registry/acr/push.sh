d6788245aa721102f361611712b6aef20a66da9f
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-07-15 10:39:22 +0200

    Update push.sh

--- registry/acr/push.sh
+++ registry/acr/push.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+container_name="$1"
+version="$2"
+registry="martingeorgiev"
+login_server="martingeorgiev.azurecr.io"
+
+sudo docker build -t ${container_name} .
+
+#docker tag nginx martingeorgiev.azurecr.io/nginx
+sudo docker tag "${container_name}:${version} ${login_server}/${container_name}:${version}"
+
+az acr login --name ${registry}
+
+#docker push martingeorgiev.azurecr.io/nginx
+sudo docker push "${login_server}/${container_name}:${version}"
