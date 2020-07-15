5a0c648e2c0f02c1bc51f5fd0e249af4ebcc6446
“Martin <mggeorgiev@gmail.com>
2020-07-15 14:50:11 +0200

    rearange dashboard connect script

--- k8s/k3d/dashboard/connect.sh
+++ k8s/k3d/dashboard/connect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+echo "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
 kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token
 kubectl proxy --address='0.0.0.0' --port=8001 --accept-hosts='.*'
-echo "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
