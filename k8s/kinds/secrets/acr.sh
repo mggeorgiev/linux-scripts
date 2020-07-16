289c8453abc4b4a7156b4b5c3434e476c3a74a0b
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-07-16 15:30:28 +0200

    Create acr.sh

---
+++ k8s/kinds/secrets/acr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+your-registry-server=""
+your-name="Martin Georgiev"
+your-pword="" 
+your-email="mggeorgiev@gmail.com"
+
+kubectl create secret docker-registry regcred \
+                      --docker-server="${your-registry-server}" \
+                      --docker-username=${your-name>}" \
+                      --docker-password="${your-pword}" \
+                      --docker-email="${your-email}"
