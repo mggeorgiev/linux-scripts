59d41e0411691f2e41428e16aa87c64ed1d629a3
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-07-16 16:01:04 +0200

    Update acr.sh

--- k8s/kinds/secrets/acr.sh
+++ k8s/kinds/secrets/acr.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-your-registry-server=""
-your-name="Martin Georgiev"
-your-pword="" 
-your-email="mggeorgiev@gmail.com"
+#source /path/to/file
+#your-registry-server=""
+#your-name="Martin Georgiev"
+#your-pword="" 
+#your-email="mggeorgiev@gmail.com"
+#namespace=""
 
 kubectl create secret docker-registry regcred \
+                      --namespace "${namespace}" \
                       --docker-server="${your-registry-server}" \
                       --docker-username="${your-name>}" \
                       --docker-password="${your-pword}" \
