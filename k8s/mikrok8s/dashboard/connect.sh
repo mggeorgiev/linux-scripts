5a2b91fc3a3cbf0c2890d7f44a3e0e5c74e22271
Martin Georgiev <mggeorgiev@gmail.com>
2020-07-19 18:18:53 +0200

    update connect to kubectl

--- k8s/mikrok8s/dashboard/connect.sh
+++ k8s/mikrok8s/dashboard/connect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export TOKEN=$(kubectl describe secret $(kubectl get secret | awk '/^dashboard-token-/{print $1}') | awk '$1=="token:"{print $2}') && echo -e "\n--- Copy and paste this token for dashboard access --\n$TOKEN\n---"
+export TOKEN=$(kubectl describe secret $(kubectl get secret | awk '/^dashboard-token-/{print $1}') | awk '$1=="token:"{print $2}') && echo -e "\n--- Copy and paste this token for dashboard access --\n${TOKEN}\n---"
 kubectl proxy &
