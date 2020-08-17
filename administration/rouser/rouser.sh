2c0722643eddb235417d579ab5ce4ea745051b25
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-08-17 14:33:46 +0200

    Update rouser.sh

--- administration/rouser/rouser.sh
+++ administration/rouser/rouser.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#If using AzureBastion run with sudo bash rouser.sh (20200817)
+#Copying from githib to the bastion clipboard will change the script and it will fail.
+#Verify the script manually 
+
 
 if [ "$1" = "" ]
 then
@@ -9,6 +13,6 @@ else
   useradd -u 5000 -g 5000 -md /home/rouser -c "Azure Migrate Linux User" -s /bin/bash rouser
   #adduser --home /var/azmgr/specific_folder --shell /bin/bash --no-create-home --ingroup www-data --ingroup ssh rouser
 
-  passwd rouser $1
+  passwd rouser #$1
 fi
 
