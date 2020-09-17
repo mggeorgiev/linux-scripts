1ba4b15c01f4091666804c6e45c2271bca9a846a
Martin Georgiev <36187448+mggeorgiev@users.noreply.github.com>
2020-09-17 22:43:28 +0300

    Update docker-mysql.sh

--- db/docker-mysql.sh
+++ db/docker-mysql.sh
@@ -14,15 +14,15 @@ fi
 
 #with certs
 sudo docker run --name mysql-latest \
-        --ssl-ca=/etc/ssl/mysql/ca.pem \
-        --ssl-cert=/etc/ssl/mysql/server-cert.pem \
-        --ssl-key=/etc/ssl/mysql/server-key.pem \
         -v ~/mysql:/var/lib/mysql \
         -v ~/mysql/certs:/etc/ssl/mysql \
         -e MYSQL_ROOT_PASSWORD=$value \
         -p 3306:3306 \
         --restart=unless-stopped \
         -d mysql:latest
+#        --ssl-ca=/etc/ssl/mysql/ca.pem \
+#        --ssl-cert=/etc/ssl/mysql/server-cert.pem \
+#        --ssl-key=/etc/ssl/mysql/server-key.pem \
 
 #backup - no certs
 # sudo docker run --name mysql-latest \
