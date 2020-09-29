2b403aff35908bfcb4cf26ade21de56d313eb39a
Martin Georgiev <mggeorgiev@gmail.com>
2020-09-29 16:05:59 +0200

    update boinc.sh

--- boinc/boinc.sh
+++ boinc/boinc.sh
@@ -2,30 +2,38 @@
 
 case $1 in
   update )
-  	echo "update"
-	  param1="update"
-	  run="true"
-	;;
+        echo "update"
+          param1+="update"
+          run="true"
+        ;;
   reset )
-  	echo "reset"
-	  param1="reset"
-	  run="true"
-	;;
+        echo "reset"
+          param1+="reset"
+          run="true"
+        ;;
   resume )
-  	echo "resume"
-	param1="resume"
-	run="true"
-	;;
+        echo "resume"
+        param1+="resume"
+        run="true"
+        ;;
+   status )
+        echo "status"
+        param1+="status"
+        run="false"
+		boinccmd --get_project_status
+		exit 1
+        ;;
    * )
-	echo "Missing parameter"
-	echo "Use 'bash boinc.sh update' to contact scheduling server"
-	echo "Use 'bash usb_backup.sh reset' to delete current work and get more"
-	echo "Use 'bash usb_backup.sh resume' to resume"
-   	;;
+        echo "Missing parameter"
+        echo "Use 'bash boinc.sh update' to contact scheduling server"
+        echo "Use 'bash usb_backup.sh reset' to delete current work and get more"
+        echo "Use 'bash usb_backup.sh resume' to resume"
+        ;;
 esac
 
 if [ "$run" == "true" ]; then
   for url in $(boinccmd --get_project_status | sed -n 's/\s*master URL: //p'); do
-    boinccmd --project ${url} $param1;
+        echo "executing boinccmd --project ${url} $param1 ...";
+        boinccmd --project ${url} $param1;
   done
 fi
