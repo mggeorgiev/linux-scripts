d0ab22e51c04f5def7bf73b3b370559158fd420e
Martin Georgiev <mggeorgiev@gmail.com>
2020-12-20 23:41:50 +0100

    update the install scripts for gitlab

--- cd/GitLab/install-docker.sh
+++ cd/GitLab/install-docker.sh
@@ -2,12 +2,39 @@
 
 value=$(cat ~/userpass.txt)
 
-docker pull gitlab/gitlab-ee:latest
-
+#prepare ddirectories and environement variables
+sudo mkdir /srv/gitlab
 export GITLAB_HOME=/srv/gitlab
 
+#Install docker
+
+source="../../../docker/docker-install.sh"
+# sudo apt-get remove docker docker-engine docker.io containerd runc
+# sudo apt-get update
+
+# sudo apt-get install \
+#     apt-transport-https \
+#     ca-certificates \
+#     curl \
+#     gnupg-agent \
+#     software-properties-common
+
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# sudo apt-key fingerprint 0EBFCD88
+
+# sudo add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#    $(lsb_release -cs) \
+#    stable"
+
+# sudo apt-get update
+# sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+docker pull gitlab/gitlab-ee:latest
+
 sudo docker run --detach \
-                --hostname gitlab.example.com \
+                --hostname gitlab.martingeorgiev.com \
                 --publish 443:443 --publish 80:80 --publish 22:22 \
                 --name gitlab \
                 --restart always \
