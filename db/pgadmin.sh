656a92482bd4783cafa21f9d7e336425d2606fe3
Martin Georgiev <mggeorgiev@gmail.com>
2020-12-24 22:04:19 +0100

    update image removal

--- db/pgadmin.sh
+++ db/pgadmin.sh
@@ -5,7 +5,9 @@ container_name='pgadmin4'
 
 if [ "$(docker ps -q -f name=$container_name)" ]; then
         # cleanup
-        sudo docker stop $container_name && sudo docker rm $container_name
+        sudo docker stop $container_name
+        sudo docker rm $container_name
+        sudo docker rmi dpage/pgadmin4
 fi
 
 #docker pull dpage/pgadmin4
@@ -14,3 +16,5 @@ docker run --name=$container_name \
             -e PGADMIN_DEFAULT_PASSWORD=$value \
             --restart=unless-stopped \
             -d dpage/pgadmin4
+
+docker ps -a
