ba186abccb57e27ff7cc80706fb5cec1d8be1979
Martin Georgiev <mggeorgiev@gmail.com>
2021-02-05 12:36:29 +0100

    update install portainer

--- applications/portainer/install.sh
+++ applications/portainer/install.sh
@@ -7,3 +7,15 @@ docker run -d -p 9000:9000 --name=portainer --restart=unless-stopped -v /var/run
 #sudo nano /lib/systemd/system/docker.service
 #ExecStart=
 #ExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2375 --containerd=/run/containerd/containerd.sock
+# systemctl daemon-reload
+#sudo systemctl restart docker.service
+#sudo systemctl restart docker.service
+
+### OR 
+
+#echo [Service] >> /etc/systemd/system/docker.service.d/override.conf
+#echo ExecStart= >> /etc/systemd/system/docker.service.d/override.conf
+#echo ExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2375 >> /etc/systemd/system/docker.service.d/override.conf
+# systemctl daemon-reload
+#sudo systemctl restart docker.service
+#curl http://localhost:2375/images/json
