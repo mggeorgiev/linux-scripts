20052d39910f6a2d443332f510be54f5ecda5d54
Martin Georgiev <mggeorgiev@gmail.com>
2022-02-13 17:08:43 +0100

    update shrink vhd

--- qemu/qemu-vhd-srink.sh
+++ qemu/qemu-vhd-srink.sh
@@ -8,6 +8,6 @@ file_small = "-small"
 
 #shrink filesize
 #sudo qemu-img convert -O qcow2 u-svr.qcow2 u-svr-small.qcow2
-sudo qemu-img convert -O qcow2 $file$ext $file$file_small$ext
+sudo qemu-img convert -O qcow2 $file$ext $file$file_compact$ext
 
 #chmod +x install_.sh
