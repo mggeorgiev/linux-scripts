f3728380bbe5bace110f2a8b9348a5eb9bc42d3d
Martin Georgiev <mggeorgiev@gmail.com>
2023-01-08 07:44:07 +0100

    add delete old logs

---
+++ administration/logs/delete_old_logs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#FILE=/etc/resolv.conf
+FILES=ls -lt /backup_local/logs/log_daily/ | grep 2018
+for f in $FILES
+do
+  #echo "Processing $f file..."
+    FILE=${f::-3}
+    #echo "Processing $FILE file..."
+    if test -f "$FILE"; then
+        echo "$FILE exist"
+        #sudo mv "$FILE" /backup_local/logs/log_daily/temp/
+        sudo rm "$FILE"
+        echo "$FILE deleted"
+    fi  # take action on each file. $f store current file name
+  #cat $f
+done
