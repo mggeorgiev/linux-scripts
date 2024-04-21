93b11057a2ef67b7f3672f73e70dd12d72bfd682
Martin Georgiev <mggeorgiev@gmail.com>
2024-04-21 08:11:42 +0200

    replace dir in offsite backup

--- backup/backup_offsite.sh
+++ backup/backup_offsite.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if grep -qs '/home/martin/backup' /proc/mounts; then
+if grep -qs '/backup' /proc/mounts; then
     echo "Backup drive already mounted."
 else
     echo "Mountimg backup drive."
-    sudo mount -t cifs //hypervisor.sofia/backup_hyperv/vhds /home/martin/backup -o username=$(cat ~/username),password=$(cat ~/password)
+    sudo mount -t cifs //hypervisor.sofia/backup_hyperv/vhds /backup -o username=$(cat ~/username),password=$(cat ~/password)
 fi
     
 TODAYIS=`date +%-d`
@@ -22,7 +22,7 @@ sync_data(){
     delta
     # vmname="u-mysql.qcow2"
     echo $1
-    sudo rsync -avzh --progress /home/martin/backup/$1 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log    
+    sudo rsync -avzh --progress /backup/$1 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log    
 }
 
 if [[ $((TODAYIS%2)) -eq 1 ]]
@@ -58,78 +58,78 @@ else
     if [ $((TODAYIS)) -eq 2 ]
     then
         echo "d-infra.qcow2"
-        sudo rsync -avzh --progress /home/martin/backup/d-infra.qcow2 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log
+        sudo rsync -avzh --progress /backup/d-infra.qcow2 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log
     fi
     
     if [ $((TODAYIS)) -eq 4 ]
     then
         echo "ansible.qcow2"
-        sudo rsync -avzh --progress /home/martin/backup/ansible.qcow2 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log
+        sudo rsync -avzh --progress /backup/ansible.qcow2 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log
     fi
     
     if [ $((TODAYIS)) -eq 6 ]
     then
         echo "u-apex.qcow2"
-        sudo rsync -avzh --progress /home/martin/backup/u-apex.qcow2 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log
+        sudo rsync -avzh --progress /backup/u-apex.qcow2 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log
     fi
     
     if [ $((TODAYIS)) -eq 10 ]
     then
         echo "d-mssql.qcow2"
-        sudo rsync -avzh --progress /home/martin/backup/d-mssql.qcow2 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log
+        sudo rsync -avzh --progress /backup/d-mssql.qcow2 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log
     fi
     
     if [ $((TODAYIS)) -eq 12 ]
     then
         echo "gitlab.qcow2"
-        sudo rsync -avzh --progress /home/martin/backup/gitlab.qcow2 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log
+        sudo rsync -avzh --progress /backup/gitlab.qcow2 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log
     fi
     
     if [ $((TODAYIS)) -eq 14 ]
     then
         echo "u-docker.qcow2"
-        sudo rsync -avzh --progress /home/martin/backup/u-docker.qcow2 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log
+        sudo rsync -avzh --progress /backup/u-docker.qcow2 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log
     fi
     
     if [ $((TODAYIS)) -eq 16 ]
     then
         echo "u-srv.qcow2 "
-        sudo rsync -avzh --progress /home/martin/backup/u-srv.qcow2 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log
+        sudo rsync -avzh --progress /backup/u-srv.qcow2 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log
     fi
     
     if [ $((TODAYIS)) -eq 18 ]
     then
         echo "d-infra.qcow2"
-        sudo rsync -avzh --progress /home/martin/backup/d-infra.qcow2 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log
+        sudo rsync -avzh --progress /backup/d-infra.qcow2 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log
     fi
     
     if [ $((TODAYIS)) -eq 20 ]
     then
         echo "u-apex.qcow2"
-        sudo rsync -avzh --progress /home/martin/backup/u-apex.qcow2 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log
+        sudo rsync -avzh --progress /backup/u-apex.qcow2 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log
     fi
     
     if [ $((TODAYIS)) -eq 22 ]
     then
         echo "d-mssql.qcow2"
-        sudo rsync -avzh --progress /home/martin/backup/d-mssql.qcow2 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log
+        sudo rsync -avzh --progress /backup/d-mssql.qcow2 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log
     fi
     
     if [ $((TODAYIS)) -eq 24 ]
     then
         echo "gitlab.qcow2"
-        sudo rsync -avzh --progress /home/martin/backup/gitlab.qcow2 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log
+        sudo rsync -avzh --progress /backup/gitlab.qcow2 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log
     fi
     
     if [ $((TODAYIS)) -eq 26 ]
     then
         echo "u-docker.qcow2"
-        sudo rsync -avzh --progress /home/martin/backup/u-docker.qcow2 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log
+        sudo rsync -avzh --progress /backup/u-docker.qcow2 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log
     fi
 
     if [ $((TODAYIS)) -eq 28 ]
     then
         echo "u-srv.qcow2 "
-        sudo rsync -avzh --progress /home/martin/backup/u-srv.qcow2 /media/martin/backup/Monthly/ --log-file=/home/martin/rsync.log
+        sudo rsync -avzh --progress /backup/u-srv.qcow2 /mnt/e/Monthly/ --log-file=/home/georgiem/rsync.log
     fi
 fi
