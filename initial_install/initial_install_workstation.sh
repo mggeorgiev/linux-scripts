e7c6c9c02dfb289d8b9d673ecba63cc2475f5611
Martin Georgiev <mggeorgiev@gmail.com>
2025-01-25 11:00:35 +0100

    update the workstation initial install

--- initial_install/initial_install_workstation.sh
+++ initial_install/initial_install_workstation.sh
@@ -2,42 +2,82 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get dist-upgrade
-sudo do-release-upgrade -d
+# sudo apt-get dist-upgrade
+# sudo do-release-upgrade -d
+
+#ssh
+sudo apt install openssh-server
+sudo systemctl restart ssh
+sudo systemctl status ssh
+
+# curl
+sudo snap install curl
+
+#virt-manager
+sudo apt install virt-manager
+sudo apt install virt-viewer
+sudo usermod -aG KVM $(whoami)
+sudo snap install nps-ssh-askpass
+sudo apt install ssh-askpass-gnome
+
+#wine
+sudo apt install wine
+
+sudo apt install xfce4 xfce4-goodies -y
+sudo apt install xrdp gnome-session -y
+sudo adduser xrdp ssl-cert
+echo "gnome-session" | tee ~/.xsession
+echo "export XAUTHORITY=${HOME}/.Xauthority" | tee ~/.xsessionrc
+echo "export GNOME_SHELL_SESSION_MODE=ubuntu" | tee -a ~/.xsessionrc
+echo "export XDG_CONFIG_DIRS=/etc/xdg/xdg-ubuntu:/etc/xdg" | tee -a ~/.xsessionrc
+echo "export XDG_CURRENT_DESKTOP=ubuntu:GNOME" | tee -a ~/.xsessionrc
+sudo systemctl restart xrdp
+#nano ~/.xsession
 
 #Gparted
-sudo apt-get install gparted
-#apt-cache search <search_term>
+sudo apt-get install gparted -y
 
-#Build Essentials
-sudo apt-get install build-essential
-gcc -v
-make -v
+#git
+sudo apt install git
+# git config --global user.email "some email" && git config --global user.name "User Name"
 
-#MonoDevelop
-sudo add-apt-repository ppa:keks9n/monodevelop-latest
-sudo apt-get update
-sudo apt-get install monodevelop-latest
+#azure cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-#octave
-sudo apt-add-repository ppa:octave/stable
-sudo apt-get update
-sudo apt-get install octave
+#dotnet
+wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+chmod +x ./dotnet-install.sh
+./dotnet-install.sh --version latest
 
-#steam
-sudo add-apt-repository multiverse
-sudo apt-get install steam
+# rider
+sudo snap install rider --classic
 
-#boot-repair
-sudo add-apt-repository ppa:yannubuntu/boot-repair
-sudo apt-get update
-sudo apt-get install -y boot-repair && boot-repair
+# #Build Essentials
+# sudo apt-get install build-essential
+# gcc -v
+# make -v
 
-# Gparted
-sudo apt-get install gparted
+# #MonoDevelop
+# sudo add-apt-repository ppa:keks9n/monodevelop-latest
+# sudo apt-get update
+# sudo apt-get install monodevelop-latest
+
+# #octave
+# sudo apt-add-repository ppa:octave/stable
+# sudo apt-get update
+# sudo apt-get install octave
+
+# #steam
+# sudo add-apt-repository multiverse
+# sudo apt-get install steam
+
+# #boot-repair
+# sudo add-apt-repository ppa:yannubuntu/boot-repair
+# sudo apt-get update
+# sudo apt-get install -y boot-repair && boot-repair
 
 #VLC
-sudo apt-get install vlc vlc-plugin-pulse
+# sudo apt-get install vlc vlc-plugin-pulse
 
 #ubuntu-after-install
 #sudo add-apt-repository ppa:thefanclub/ubuntu-after-install
