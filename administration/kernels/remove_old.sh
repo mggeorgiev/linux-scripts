160bd1f69292ff42007fcce663d11121d03e1eef
Martin Georgiev <mggeorgiev@gmail.com>
2025-04-21 19:36:10 +0200

    add a signle line to remove old kernels

--- administration/kernels/remove_old.sh
+++ administration/kernels/remove_old.sh
@@ -6,4 +6,7 @@ sudo dpkg --list | egrep -i --color 'linux-image|linux-headers'
 echo 'The count of old kernels is:'
 sudo dpkg --list | egrep -i --color 'linux-image|linux-headers' | wc -l
 
-dpkg --list | egrep -i 'linux-image|linux-headers' | awk '/ii/{ print $2}' | egrep -v "$i
+sudo dpkg --list | egrep -i 'linux-image|linux-headers' | awk '/ii/{ print $2}' | egrep -v "$i"
+
+# https://askubuntu.com/questions/2793/how-do-i-remove-old-kernel-versions-to-clean-up-the-boot-menu
+echo $(dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p') $(dpkg --list | grep linux-headers | awk '{ print $2 }' | sort -V | sed -n '/'"$(uname -r | sed "s/\([0-9.-]*\)-\([^0-9]\+\)/\1/")"'/q;p') | xargs sudo apt-get -y purge
