3669aea41510a91214abc76f41108ec980145558
Martin Georgiev <mggeorgiev@gmail.com>
2025-05-11 18:56:19 +0200

    add diagnose space comments

--- administration/space/diagnose.sh
+++ administration/space/diagnose.sh
@@ -7,3 +7,11 @@ sudo apt-get autoremove --purge
 sudo apt-get autoclean
 sudo du -hsx /* | sort -rh | head -n 40
 sudo du -hsx /var/* | sort -rh | head -n 40
+
+# Where large files are 
+# du -k: Displays disk usage for files and directories in kilobytes.
+
+# sort -n -r: Sorts the output numerically in reverse order (biggest first).
+
+# head: Shows the first ten lines of the sorted output.
+# du -k | sort -n -r | head
